b879dddcb7452637641dfb7f7c1a050812ce5a65
Ch1n <anhngorrm8cms@gmail.com>
2026-01-08 15:10:32 +0800

    cpoclib

--- pkg/templates/code_invoke/code.go
+++
@@ -1,9 +0,0 @@
-package code_invoke
-
-import (
-	"github.com/AduraK2/cpoclib/pkg/templates/code_model"
-)
-
-func CodeInvoke(src string, codeParams []code_model.CodeParams) (dst string, err error) {
-	return
-}
